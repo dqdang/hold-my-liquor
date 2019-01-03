b4ed35c19bfb44c7d72e9b075f2d1196be49335a
Derek Dang <dqdang17@gmail.com>
2019-01-02 21:01:56 -0800

    stopping while loop for debugging

--- yelp.py
+++ yelp.py
@@ -38,33 +38,33 @@ def get_current():
     
     failures = 0
 
-    while True:
+    # while True:
 
-        print("Checking if new products are on ACRNM on proxy: {}".format(site.proxy_used))
-        if not site.get().ok:
-            print("Proxy or website is unresponsive. Trying again...")
-            failures += 1
-            site.proxy_used = site.sockets.pop(0)
-            continue
-        else:
-            failures = 0
+    #     print("Checking if new products are on ACRNM on proxy: {}".format(site.proxy_used))
+    #     if not site.get().ok:
+    #         print("Proxy or website is unresponsive. Trying again...")
+    #         failures += 1
+    #         site.proxy_used = site.sockets.pop(0)
+    #         continue
+    #     else:
+    #         failures = 0
         
-        tree = html.fromstring(str(site))
-        tree.make_links_absolute(url)
+    #     tree = html.fromstring(str(site))
+    #     tree.make_links_absolute(url)
 
-        prod_names = tree.xpath("//div[@class='name']/text()")
-        prod_urls = tree.xpath("//a[contains(concat(' ', normalize-space(@class), ' '), ' tile ')]/@href")
+    #     prod_names = tree.xpath("//div[@class='name']/text()")
+    #     prod_urls = tree.xpath("//a[contains(concat(' ', normalize-space(@class), ' '), ' tile ')]/@href")
 
-        new, restock = db.new_items(prod_names, prod_urls)
+    #     new, restock = db.new_items(prod_names, prod_urls)
 
-        if new:
-            new = list(zip(*new))
-            notify(new[1], restock)
-            db.insert_products(new[0])
-        else:
-            notify(new, restock)
+    #     if new:
+    #         new = list(zip(*new))
+    #         notify(new[1], restock)
+    #         db.insert_products(new[0])
+    #     else:
+    #         notify(new, restock)
 
-        db.insert_current(prod_names, prod_urls)
+    #     db.insert_current(prod_names, prod_urls)
                 
 if  __name__ == "__main__":
     db.create_tables()
