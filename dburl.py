06abb8911d120866d4e88a9363fd9debd75a7c89
Derek Dang <dqdang17@gmail.com>
2019-01-03 23:20:57 -0800

    remove dburl

--- dburl.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['DATABASE_URL'] = \
-"postgres://byctvicvfddijo:c47907076f520775f2fa8ad020d61ac4bcb39d2ad91a5420b90ea192483b8697@ec2-54-225-150-216.compute-1.amazonaws.com:5432/d3mv68kahub9jv
-"
