10e70009db78c989a577a8f61b974d2de7e69f18
Derek Dang <dqdang17@gmail.com>
2019-01-25 13:29:41 -0800

    formatting

--- yelp.py
+++ yelp.py
@@ -7,7 +7,6 @@ import requests
 import dbhandler as db
 import yppage as yp
 
-TIMEOUT = 600
 def get_results(query):
     if len(query) > 1:
         search_results = yp.yelp_api.search_query(term=query[0], location=query[1], sort_by='best_match', limit=1)
@@ -15,11 +14,11 @@ def get_results(query):
         search_results = yp.yelp_api.search_query(term=query[0], location="san jose", sort_by='best_match', limit=1)
     return search_results
 
-
-def pingme(timeout):
+def pingme():
     start = time.time()
     while True:
         time.sleep(60 - ((time.time() - start) % 60.0))
+        page.send(os.environ['SECRET_USER'], "wingstop")
+
 if  __name__ == "__main__":
     db.create_tables()
-    pingme(TIMEOUT)
