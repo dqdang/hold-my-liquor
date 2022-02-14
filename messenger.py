506932043a546a6dd50caed8f572591fc9f0f672
Derek.Dang <dqdang17@gmail.com>
2022-02-14 00:33:45 -0800

    messenger: Use fbmessenger instead of fbmq

--- messenger.py
+++ messenger.py
@@ -1,5 +1,5 @@
-from fbpage import page
-from fbmq import QuickReply, Template
+from flask import Flask, request
+from fbmessenger import BaseMessenger
 from zipcode import search
 
 import os
@@ -7,9 +7,6 @@ import re
 import requests
 import dbhandler as db
 import yelp as yelp
-
-page.greeting("Click Get Started below to get started!")
-page.show_starting_button("Get Started")
 
 
 def general_query(results):
@@ -23,24 +20,9 @@ def general_query(results):
     return rv
 
 
-@page.handle_postback
-def received_postback(event):
-    sender_id = event.sender_id
-    recipient_id = event.recipient_id
-    time_of_postback = event.timestamp
-    payload = event.payload
-
-    page.typing_on(sender_id)
-    page.send(sender_id, "Welcome! Search yelp for something like this:\nDISH, LOCATION\n\nChange default location with location=LOCATION")
-    db.insert_user(sender_id)
-    page.typing_off(sender_id)
-
-
-@page.handle_message
-def message_handler(event):
-    sender_id = event.sender_id
+def process_message(message):
     try:
-        message = event.message.get('text').lower()
+        message = message['message']['text'].lower()
         if("=" in message):
             message = message.split("=")[-1]
             if(message.isdigit()):
@@ -48,44 +30,72 @@ def message_handler(event):
             message = re.sub("[\W+]", " ", message.upper())
             message = message.strip()
             db.change_location(sender_id, message)
-            send = "Changed default location to {}".format(message)
+            send = Text(text="Changed default location to {}".format(message))
             page.send(sender_id, send)
-            return
+            return response.to_dict()
         message = re.sub("[\W+]", " ", message.upper())
         message = message.strip()
         if("CURRENT LOCATION" in message):
+            send = Text(text=db.get_location(sender_id))
             page.send(sender_id, db.get_location(sender_id))
-            return
+            return response.to_dict()
     except:
         return
 
     if not message:
         return
 
-    # "hot pot, san francisco" OR "hot pot"
     split = message.split("  ")
     results = yelp.get_results(sender_id, split)
     rv = general_query(results)
-    page.send(sender_id, rv)
-    return "Message processed"
+    return response.to_dict()
 
 
-@page.handle_delivery
-def received_delivery_confirmation(event):
-    delivery = event.delivery
-    message_ids = delivery.get("mids")
-    watermark = delivery.get("watermark")
+class Messenger(BaseMessenger):
+    def __init__(self, page_access_token):
+        self.page_access_token = page_access_token
+        super(Messenger, self).__init__(self.page_access_token)
+
+    def message(self, message):
+        action = process_message(message)
+        if action:
+            res = self.send(action, 'RESPONSE')
+
+    def postback(self, message):
+        payload = message['postback']['payload']
+        sender_id = self.get_user_id()
+        page.typing_on(sender_id)
+        page.send(sender_id, "Welcome! Search yelp for something like this:\nDISH, LOCATION\n\nChange default location with location=LOCATION")
+        db.insert_user(sender_id)
+        page.typing_off(sender_id)
+
+    def init_bot(self):
+        self.add_whitelisted_domains('https://facebook.com/')
+        greeting = GreetingText(text='Get started with the below button!.')
+        self.set_messenger_profile(greeting.to_dict())
+
+        get_started = GetStartedButton(payload='Get Started')
+        self.set_messenger_profile(get_started.to_dict())
 
 
-@page.handle_read
-def received_message_read(event):
-    watermark = event.read.get("watermark")
-    seq = event.read.get("seq")
+app = Flask(__name__)
+app.debug = True
+messenger = Messenger(os.environ.get('FB_PAGE_TOKEN'))
 
 
-@page.handle_echo
-def received_echo(event):
-    message = event.message
-    message_id = message.get("mid")
-    app_id = message.get("app_id")
-    metadata = message.get("metadata")
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
+            if request.args.get('init') and request.args.get('init') == 'true':
+                messenger.init_bot()
+                return ''
+            return request.args.get('hub.challenge')
+        raise ValueError('FB_VERIFY_TOKEN does not match.')
+    elif request.method == 'POST':
+        messenger.handle(request.get_json(force=True))
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
