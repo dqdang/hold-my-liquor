7e538807f72f086570e1d88e7bd51b8fa7470a6b
Derek Dang <dqdang@users.noreply.github.com>
2022-02-21 22:41:36 -0800

    messenger: Add debug statement

--- messenger.py
+++ messenger.py
@@ -28,7 +28,12 @@ def get_results(sender_id, query):
         search_results = yelp_api.search_query(
             term=query[0], location=query[1], sort_by='best_match', limit=1)
     else:
-        search_results = yelp_api.search_query(
+        try:
+            search_results = yelp_api.search_query(
+            term=query[0], location=db.get_location(sender_id), sort_by='best_match', limit=1)
+        except:
+            print(db.get_location(sender_id))
+            yelp_api.search_query(
             term=query[0], location=db.get_location(sender_id), sort_by='best_match', limit=1)
     return search_results
 
